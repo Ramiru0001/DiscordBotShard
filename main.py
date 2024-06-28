296779777ee14a4f1f55c2b94d8e77c96b28fbde
Ramiru0001 <shian2528@gmail.com>
2024-06-28 16:42:31 +0900

    あ

--- main.py
+++ main.py
@@ -757,8 +757,9 @@ async def on_disconnect():
 # !current_time コマンドの定義
 @client.command(name='current_time', help='Shows current time in scheduler.')
 async def current_time(ctx):
-    current_scheduler_time = scheduler.get_current_time()  # スケジューラの現在時刻を取得する関数を実装する
-    await ctx.send(f'Current time in scheduler: {current_scheduler_time}')
+    #current_scheduler_time = scheduler.get_current_time()  # スケジューラの現在時刻を取得する関数を実装する
+    #await ctx.send(f'Current time in scheduler: {current_scheduler_time}')
+    await ctx.send(f"{datetime.now(jst).strftime('%Y-%m-%d %H:%M:%S %Z')}")
 
 @client.command(name='ping')
 async def ping(ctx):
